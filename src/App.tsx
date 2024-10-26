949a1ea54b0fc5fd28a28e255cafe624e7336fa6
habilis <data.noiphap@gmail.com>
2024-10-26 13:33:01 +0700

    refactor: Refactor App.tsx to remove commented out code and use useMemo hook in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
 import Graph from './components/graphs/index';
 import { GraphEdge, GraphNode } from 'reagraph';
 import NodeService from './services/NodeService';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 
 function App() {
     const [nodes, setNodes] = useState<GraphNode[]>([]);
-
-    // const nodes = [
-    //     {
-    //         id: '1',
-    //         label: '1'
-    //     },
-    //     {
-    //         id: '2',
-    //         label: '2'
-    //     },
-    //     {
-    //         id: '3',
-    //         label: '????'
-    //     }
-    // ];
 
     const edges: GraphEdge[] = [
         {
             source: '6c8620e467404fb68f8379b70b3d50da',
             target: '6c08a3900d024a2196892c0f6f10d57d',
-            id: '1-2',
-            label: '1-2'
-        },
-        // {
-        //     source: '2',
-        //     target: '1',
-        //     id: '2-1',
-        //     label: '2-1'
-        // },
-        // {
-        //     source: '1',
-        //     target: '3',
-        //     id: '1-3',
-        //     label: '1-3'
-        // }
+            id: '1-2'
+        }
     ];
 
     useMemo(() => {
